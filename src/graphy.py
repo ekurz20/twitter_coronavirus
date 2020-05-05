13b95bfb941f21083cedf6a93412135375f1bd39
ekurz20 <ekurz20@cmc.edu>
2020-05-05 16:46:18 -0700

    test

--- src/graphy.py
+++ src/graphy.py
@@ -45,15 +45,14 @@ for path in args.input_paths:
                 JP.append(v)
     dates.append(date)
 
-
 plt.plot(US, label = 'United States')
 plt.plot(CN, label = 'China')
-plt.plot(IT, label = 'Italy')
-plt.plot(FR, label = 'France')
-plt.plot(ES, label = 'Spain')
-plt.plot(JP, label = 'Japan')
+#plt.plot(IT, label = 'Italy')
+#plt.plot(FR, label = 'France')
+#plt.plot(ES, label = 'Spain')
+#plt.plot(JP, label = 'Japan')
 
-plt.title("Use of hashtags related to Coronavirus/Sick by country")
+plt.title("Use of hashtags related to Coronavirus by country")
 plt.xlabel("Dates")
 plt.ylabel("Occurances")
 #z = np.array(len(dates))
