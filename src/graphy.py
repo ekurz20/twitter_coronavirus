be6f6c14b102a87b598e03c1844d0d6351099a41
ekurz20 <ekurz20@cmc.edu>
2020-05-05 17:35:04 -0700

    final graph again

--- src/graphy.py
+++ src/graphy.py
@@ -68,4 +68,7 @@ m = 0.2
 s = maxsize/plt.gcf().dpi*N+2*m
 margin = m/plt.gcf().get_size_inches()[0]
 
+plt.gcf().subplots_adjust(left=margin, right=1.-margin)
+plt.gcf().set_size_inches(s, plt.gcf().get_size_inches()[1])
+
 plt.savefig('graph.png')
